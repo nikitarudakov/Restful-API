6aa652ad1182e7cf0a9c466a7dda69d2db6814ed
foxstudent106092 <nik.datascience@gmail.com>
2023-11-01 08:37:46 +0000

    fix: remove controllers from registry

--- internal/registry/app.go
+++ internal/registry/app.go
@@ -1,25 +1,22 @@
 package registry
 
 import (
-	"git.foxminded.ua/foxstudent106092/user-management/internal/adapter/controller"
+	"git.foxminded.ua/foxstudent106092/user-management/config"
+	repoAdapter "git.foxminded.ua/foxstudent106092/user-management/internal/adapter/repository"
+	"git.foxminded.ua/foxstudent106092/user-management/internal/usecase/repository"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type registry struct {
-	db *mongo.Client
+type Registry struct {
+	Ur repository.UserRepository
+	Pr repository.ProfileRepository
+	Ar repository.AdminRepository
 }
 
-type Registry interface {
-	NewAppController() controller.AppController
-}
-
-func NewRegistry(db *mongo.Client) Registry {
-	return &registry{db: db}
-}
-
-func (r *registry) NewAppController() controller.AppController {
-	return controller.AppController{
-		User:  r.NewUserController(),
-		Admin: r.NewAdminController(),
+func NewRegistry(db *mongo.Database, dbCfg *config.Database) *Registry {
+	return &Registry{
+		Ur: repoAdapter.NewUserRepository(db.Collection(dbCfg.UserRepo)),
+		Pr: repoAdapter.NewProfileRepository(db.Collection(dbCfg.ProfileRepo)),
+		Ar: repoAdapter.NewAdminRepository(db),
 	}
 }
