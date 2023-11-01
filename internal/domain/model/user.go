33b51002167988c840c0d7734688f860974c9c21
foxstudent106092 <nik.datascience@gmail.com>
2023-11-01 08:39:32 +0000

    fix: minor type fixes + validation fix

--- internal/domain/model/user.go
+++ internal/domain/model/user.go
@@ -2,7 +2,7 @@ package model
 
 type User struct {
 	Username string `json:"username" bson:"username" validate:"required,min=4"`
-	Password string `json:"password" bson:"password" validate:"required,min=8,containsany=!@#?*"`
+	Password string `json:"password" bson:"password" validate:"required,min=8,containsany=!@#?*%"`
 }
 
 func (u *User) TableName() string { return "users" }
--- internal/usecase/usecase/user.go
+++ internal/usecase/usecase/user.go
@@ -6,7 +6,7 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-type userUsecase struct {
+type UserUsecase struct {
 	ur repository.UserRepository
 	pr repository.ProfileRepository
 }
@@ -16,17 +16,17 @@ type UserManager interface {
 	Create(u *model.User) error
 	Find(u *model.User) (*model.User, error)
 	UpdatePassword(u *model.User) error
-	CreateProfile(p *model.Profile) (interface{}, error)
-	UpdateProfile(p *model.Profile) error
+	CreateProfile(p *model.Profile, authUsername string) (interface{}, error)
+	UpdateProfile(p *model.Profile, authUsername string) error
 }
 
 // NewUserUsecase implicitly links UserManager to userUsecase struct
-func NewUserUsecase(ur repository.UserRepository, pr repository.ProfileRepository) UserManager {
-	return &userUsecase{ur: ur, pr: pr}
+func NewUserUsecase(ur repository.UserRepository, pr repository.ProfileRepository) *UserUsecase {
+	return &UserUsecase{ur: ur, pr: pr}
 }
 
 // Create creates new user and stores it in DB with repository.UserRepository
-func (uu *userUsecase) Create(u *model.User) error {
+func (uu *UserUsecase) Create(u *model.User) error {
 	_, err := uu.ur.Create(u)
 	if err != nil {
 		return err
@@ -36,7 +36,7 @@ func (uu *userUsecase) Create(u *model.User) error {
 }
 
 // Find looks up for user (u *model.User) and returns it if found
-func (uu *userUsecase) Find(u *model.User) (*model.User, error) {
+func (uu *UserUsecase) Find(u *model.User) (*model.User, error) {
 	userFromDB, err := uu.ur.Find(u)
 	if err != nil {
 		return nil, err
@@ -45,7 +45,7 @@ func (uu *userUsecase) Find(u *model.User) (*model.User, error) {
 	return userFromDB, nil
 }
 
-func (uu *userUsecase) UpdatePassword(u *model.User) error {
+func (uu *UserUsecase) UpdatePassword(u *model.User) error {
 	if err := uu.ur.UpdatePassword(u); err != nil {
 		return err
 	}
@@ -54,7 +54,22 @@ func (uu *userUsecase) UpdatePassword(u *model.User) error {
 }
 
 // CreateProfile creates profile for model.User and stores it in DB with repository.ProfileRepository
-func (uu *userUsecase) CreateProfile(p *model.Profile) (interface{}, error) {
+func (uu *UserUsecase) CreateProfile(p *model.Profile, authUsername string) (interface{}, error) {
+	if p.Nickname != authUsername {
+		var u model.User
+
+		u.Username = p.Nickname
+
+		if err := uu.ur.UpdateUsername(&u, authUsername); err != nil {
+			log.Error().Err(err).
+				Str("auth_username", authUsername).
+				Str("new_username", p.Nickname).
+				Msg("error updating username")
+
+			return nil, err
+		}
+	}
+
 	insertedID, err := uu.pr.Create(p)
 	if err != nil {
 		return nil, err
@@ -64,25 +79,25 @@ func (uu *userUsecase) CreateProfile(p *model.Profile) (interface{}, error) {
 }
 
 // UpdateProfile updates profile of model.User in DB with repository.ProfileRepository
-func (uu *userUsecase) UpdateProfile(p *model.Profile) error {
-	err := uu.pr.Update(p)
-	if err != nil {
-		return err
-	}
-
-	if p.Nickname != p.AuthUsername {
+func (uu *UserUsecase) UpdateProfile(p *model.Profile, authUsername string) error {
+	if p.Nickname != authUsername {
 		var u model.User
 
 		u.Username = p.Nickname
 
-		if err = uu.ur.UpdateUsername(&u, p.AuthUsername); err != nil {
+		if err := uu.ur.UpdateUsername(&u, authUsername); err != nil {
+			log.Error().Err(err).
+				Str("auth_username", authUsername).
+				Str("new_username", p.Nickname).
+				Msg("error updating username")
+
 			return err
 		}
+	}
 
-		log.Error().Err(err).
-			Str("auth_username", p.AuthUsername).
-			Str("new_username", p.Nickname).
-			Msg("error updating username")
+	err := uu.pr.Update(p)
+	if err != nil {
+		return err
 	}
 
 	return nil
